dca2d9fdfb61993b4b7faa92faeef68720010235
javiersorianoruiz <soriano_ruiz_javier@hotmail.com>
2022-09-15 08:56:45 +0200

    first commit user 1 fork

--- main.cpp
+++ main.cpp
@@ -2,6 +2,6 @@
 
 int main(int ac, char** av) {
 	    std::cout << "Hello World User 1 y User 2!" << std::endl;
-
+            std::cout << "change in fork user User1" << std::endl;
 	    return 0;
 }
